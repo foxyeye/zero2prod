1dbb1e1ae75bf4e61f692aa8ae3369ea6ffe6996
foxyeye <icefoxfly@gmail.com>
2025-03-31 17:44:50 +0800

    add confirm email logic

--- src/configuration.rs
+++ src/configuration.rs
@@ -42,6 +42,7 @@ pub struct DatabaseSettings {
 pub struct ApplicationSettings {
     pub port: u16,
     pub host: String,
+    pub base_url: String,
 }
 
 pub enum Environment {
---
+++ tests/api/subscriptions_confirm.rs
@@ -0,0 +1,33 @@
+use crate::helpers::spawn_app;
+use wiremock::matchers::method;
+use wiremock::{Mock, ResponseTemplate};
+
+#[actix_web::test]
+async fn confirmation_without_token_are_rejected_with_400() {
+    let app = spawn_app().await;
+
+    let response = reqwest::get(&format!("{}/subscriptions/confirm", app.address))
+        .await
+        .unwrap();
+
+    assert_eq!(response.status().as_u16(), 400);
+}
+
+#[actix_web::test]
+async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let confirmation_links = app.get_confirmation_links(&email_request);
+
+    let response = reqwest::get(confirmation_links.html).await.unwrap();
+    // Assert
+    assert_eq!(response.status().as_u16(), 200);
+}
